160c1724ddf0fae6cb26fb325842cb07f86a3f48
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2024-01-06 00:16:44 +0700

    reafactor: add component card and collapsible

--- src/Pages/Landing/TestimonialSection.jsx
+++ src/Pages/Landing/TestimonialSection.jsx
@@ -1,7 +1,7 @@
-import { ChevronLeft, ChevronRight, Star } from 'lucide-react'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
-import { getInitials } from '@/utils/string'
+import TestimonialCard from '@/Components/Card/Testimonial'
 
 const settings = {
   dots: false,
@@ -116,29 +116,8 @@ function TestimonialSection() {
         </div>
         <div className='relative mx-auto w-full max-w-6xl px-0 lg:px-8'>
           <Slider {...settings}>
-            {testimonials.map(({ name, summary }, idx) => {
-              return (
-                <div key={idx} className='flex items-center justify-center'>
-                  <div className='mx-4 mt-14 flex flex-col items-center justify-start gap-3 rounded-lg bg-white p-2 md:p-4 md:pt-10'>
-                    <div className='-mt-10 box-border flex h-16 w-16 items-center justify-center overflow-hidden rounded-full border-4 border-[#d87607] bg-[#fde58a] text-2xl tracking-wider text-[#78340f] md:-mt-16'>
-                      {getInitials(name)}
-                    </div>
-                    <h3 className='text-2xl'>{name}</h3>
-                    <p className='px-2 text-center font-raleway'>{summary}</p>
-                    <div className='flex flex-row gap-0.5 pb-3 pt-1'>
-                      {Array.from({ length: 5 }).map((_, index) => {
-                        return (
-                          <Star
-                            key={index}
-                            className='h-5 w-5 text-yellow-700'
-                          />
-                        )
-                      })}
-                      {/* <Rating initialValue={rating} disabled size='md' /> */}
-                    </div>
-                  </div>
-                </div>
-              )
+            {testimonials.map((testimonial, idx) => {
+              return <TestimonialCard key={idx} {...testimonial} />
             })}
           </Slider>
         </div>
