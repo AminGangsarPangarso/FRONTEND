b1b9e781ccfcdb90b362c57232e008ed765e1702
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2024-01-06 00:25:04 +0700

    feat: update page not found style
    Signed-off-by: Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,12 +1,20 @@
 import { Link } from 'react-router-dom'
+import { ArrowLeft } from 'lucide-react'
 
 function NotFound() {
   return (
-    <div className='flex h-full min-h-screen w-full flex-col items-center justify-center gap-2'>
-      <h1 className='text-4xl font-semibold text-stone-800'>
-        Page Not Found 404
-      </h1>
-      <Link to='/'>Back to Home</Link>
+    <div className='flex h-full min-h-screen w-full flex-col items-center justify-center gap-5'>
+      <div className='-space-y-2 text-center font-semibold text-slate-800'>
+        <h1 className='text-8xl tracking-widest'>404</h1>
+        <h1 className='text-4xl tracking-wide'>Page Not Found</h1>
+      </div>
+      <Link
+        to='/'
+        className='flex items-center gap-2 text-base underline decoration-slate-200 decoration-2 underline-offset-4'
+      >
+        <ArrowLeft className='h-4 w-4' />
+        <span>Back to Home</span>
+      </Link>
     </div>
   )
 }
